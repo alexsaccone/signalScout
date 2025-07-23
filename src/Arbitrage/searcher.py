d031b6f1bfbda7514f572513ddbb52b8841bd7b2
spiceless <saccone.alexander.c@gmail.com>
2025-07-23 03:00:25 -0400

    I am radically confident that this WORKS (calculates and filters arbitrage from dataframe)

--- src/Arbitrage/searcher.py
+++ src/Arbitrage/searcher.py
@@ -3,6 +3,7 @@ from formulas import *
 from api import *
 import json
 from datetime import datetime
+from datetime import date
 import torch
 
 import pandas as pd
@@ -96,18 +97,55 @@ def sentiment_analysis(kalshi, polymarket):
     results = []
     for i in range(k_idx):
         for j in range(p_idx):
+            prices_string = polymarket.iloc[j]["outcomePrices"]
+            string_list = json.loads(prices_string)
+            prices = [float(num) for num in string_list]
             results.append({
                 "kalshi": kalshi.iloc[i]["title"],
                 "polymarket": polymarket.iloc[j]["question"],
-                "similarity": sims[i][j]
+                "similarity": sims[i][j],
+                "oddsK_yes": kalshi.iloc[i]["yes_ask"], # ??
+                "oddsK_no": kalshi.iloc[i]["no_ask"], # ??
+                "oddsP_yes": prices[0],
+                "oddsP_no": prices[1],
+                "kalshi_close": kalshi.iloc[i]["close_time"],
+                "polymarket_close": polymarket.iloc[j]["endDateIso"]
             })
 
     df = pd.DataFrame(results).sort_values("similarity", ascending=False)
     return df
 
-def arbitrage_analysis(df, oddsA, oddsB):
-    filtered = df[df["similarity"] > 0.75]
-    arbBool = has_arbitrage(oddsA, oddsB)
+def arbitrage_analysis(df):
+    filtered_df = df[df["similarity"] > 0.75]
+    arbitrage_events = []
+    for i in filtered_df:
+        oddsK_yes = 100 / i[oddsK_yes]
+        oddsP_yes = 1 / i[oddsP_yes]
+        oddsK_no = 100 / i[oddsK_no]
+        oddsP_no = 1 / i[oddsP_no]
+
+        a_pct1 = arbitrage_pct(oddsK_yes, oddsP_no)
+        a_pct2 = arbitrage_pct(oddsK_no, oddsP_yes)
+        best_a_pct = min(a_pct1, a_pct2)
+        first_better = a_pct1 < a_pct2
+        if has_arbitrage(best_a_pct):
+            if first_better: # if betting yes on Kalshi
+                stakes = get_stakes(oddsK_yes, oddsP_no)
+            else:
+                stakes = get_stakes(oddsK_no, oddsP_yes)
+            days = (max(i["kalshi_close"], i["polymarket_close"]) - date.today()).days
+            arbitrage_events.append({
+                "kalshi_name": i["kalshi"],
+                "polymarket_name": i["polymarket"],
+                "arbitrage_pct": best_a_pct,
+                "profit_pct": profit_pct(best_a_pct),
+                "kalshi_stake": stakes[0],
+                "polymarket_stake": stakes[1],
+                "daily_pct_return": pct_return_per_day(best_a_pct, days)
+            })
+        
+    ranked_odds_df = pd.DataFrame(arbitrage_events).sort_values("Daily pct return", ascending=False)
+    return ranked_odds_df
 
 def true_match_checker(df):
         filtered = df[df["similarity"] > 0.75]
